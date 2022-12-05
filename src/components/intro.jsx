58a6f4aaf8578b6349d6aca50df8810ed4ca59f7
DevAndyMacnab <andy.macnab.j@gmail.com>
2022-12-05 09:29:23 -0600

    actualizacion

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -5,7 +5,7 @@ function Intro() {
   <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
   <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">Andy Macnab</h1>
   <p className="text-base md:text-xl mb-3 font-medium">Programador Junior y Desarrollador Web</p>
-  <p className="text-sm max-w-xl mb-6 font-bold">👋¡Hola! Soy Andy Macnab, estudiante de ingeniería en Ciencias y Sistemas en la Universidad de San Carlos de Guatemala. Me considero una persona tranquila capaz de trabajar bajo presión, apasionado por aprender y reconozco cuando necesito ayuda o que alguien mas experimentado que yo me enseñe, me gusta el trabajo en equipo, soy una persona competitiva y un poco perfeccionista, siempre busco mejorar.</p>
+  <p className="text-sm max-w-xl mb-6 font-bold">👋¡Hola! Soy Andy Macnab, estudiante de ingeniería en Ciencias y Sistemas en la Universidad de San Carlos de Guatemala. Me considero una persona tranquila capaz de trabajar bajo presión, apasionado por aprender, me gusta el trabajo en equipo, soy una persona competitiva y un poco perfeccionista, siempre busco mejorar.</p>
   </div>);
 }
 
