a02e95150975fa24cbc3cfd3a1fa6db182242d4b
DevAndyMacnab <andy.macnab.j@gmail.com>
2023-04-03 11:40:55 -0600

    new version

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -5,10 +5,7 @@ function Intro() {
   <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
   <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">Andy Macnab</h1>
   <p className="text-base md:text-xl mb-3 font-medium">Programador Junior y Desarrollador Web</p>
-  <p className="text-sm max-w-xl mb-6 font-bold">👋¡Hola! Soy Andy Macnab, estudiante de Ingeniería en Ciencias y Sistemas y Programador Junior. Desde joven me ha apasionado el mundo de la informática y la programación. Me
-considero una persona tranquila, apasionada
-por aprender, competitiva y que siempre
-busca mejorar. </p>
+  <p className="text-md max-w-xl mb-6 font-bold">👋Soy un estudiante de Ingeniería en Ciencias y Sistemas y Programador Junior con una gran pasión por la informática y la programación desde una edad temprana. Actualmente, me dedico a desarrollar proyectos personales y a trabajar como freelance, creando páginas web utilizando Python, Javascript y Wordpress. Me especializo en el desarrollo de backend en Node.Js y Python, y he adquirido habilidades para diseñar soluciones de software eficientes y escalables. Con mi pasión y experiencia en el mundo de la programación, estoy buscando oportunidades para aprender y crecer en este campo en constante evolución. </p>
   </div>);
 }
 
--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,64 +1,65 @@
 export default [{
     title: "Chat Node.Js y Socket.io",
-    imgUrl:'https://github.com/DevAndyMacnab/mi-portafolio/blob/main/public/assets/chat.png?raw=true',
-    stack:["Node.Js","Socket.io"],
+    imgUrl:'/assets/chat.png',
+    stack:["Node.Js","Socket.io", "Express", "HTML","CSS"],
     link:"https://github.com/DevAndyMacnab/Chat-NodeJs-Socket.io"
 },
 {
-    title: "API REST - MERN Stack",
-    imgUrl:'https://github.com/DevAndyMacnab/mi-portafolio/blob/main/public/assets/mern%20stack.png?raw=true',
-    stack:["Node.Js","Express.Js","React.Js","MongoDB", ],
-    link:"https://github.com/DevAndyMacnab/MERN-STACK"
+    title: "CRUD Task Manager",
+    imgUrl:'/assets/taskCRUD.png',
+    stack:["Node.Js","Express.Js","React.Js","MySQL","Vite","Tailwindcss" ],
+    link:"https://github.com/DevAndyMacnab/CRUD-API-REST"
 },
+
+{
+    title: "Aplicacion Web ChatGPT con OpenAI API",
+    imgUrl:'/assets/openai.png',
+    stack:["Node.Js","Express","OpenAI","React.JS","Vite","Tailwindcss" ],
+    link:"https://ai-app-codex-ozhej6dfp-devandymacnab.vercel.app/"
+},
+
+{
+    title: "Portal Web de Bienes Raices (En desarrollo)",
+    imgUrl:'/assets/bienesraices.png',
+    stack:["Node.Js","Express","MySQL","Pug","Sequelize","Tailwindcss","JWT" ],
+    link:"https://github.com/DevAndyMacnab/Portal-Web-Bienes-Raices"
+},
+
 {
     title: "API REST - Django & Flask",
-    imgUrl:'https://github.com/DevAndyMacnab/mi-portafolio/blob/main/public/assets/api%20flask%20django.png?raw=true',
-    stack:["Python","Django","XML","Flask"],
+    imgUrl:'/assets/api flask django.png',
+    stack:["Python","Django","XML","Flask", "OOP", "REST"],
     link:"https://github.com/DevAndyMacnab/API-REST-DJANGO-FLASK"
 },
 
 {
     title: "Pokedex with React.Js (PokeAPI)",
-    imgUrl:'https://github.com/DevAndyMacnab/mi-portafolio/blob/main/public/assets/poke%20api.png?raw=true',
+    imgUrl:'/assets/poke api.png',
     stack:["CSS","React.Js"],
     link:"https://github.com/DevAndyMacnab/pokedex-con-react"
 },
 {
-    title: "Autómata de Pila",
-    imgUrl:'https://github.com/DevAndyMacnab/mi-portafolio/blob/main/public/assets/automata%20de%20pila.png?raw=true',
-    stack:["Python","HTML","CSS","Tkinter"],
-    link:"https://github.com/DevAndyMacnab/Automata-de-Pila"
+    title: "Analizador Léxico",
+    imgUrl:'/assets/analizador lexico.png',
+    stack:["Python","Tkinter", "OOP"],
+    link:"https://github.com/DevAndyMacnab/Analizador-Lexico"
 },
-{
-    title: "Autómata Finito",
-    imgUrl:'https://github.com/DevAndyMacnab/mi-portafolio/blob/main/public/assets/automata%20de%20pila.png?raw=true',
-    stack:["Python","HTML","Tkinter"],
-    link:"https://github.com/DevAndyMacnab/Automata-Finito"
-},
-
-
-
 
 
 {
     title: "Página Web",
-    imgUrl:'https://github.com/DevAndyMacnab/mi-portafolio/blob/main/public/assets/wordpress.png?raw=true',
+    imgUrl:'/assets/wordpress.png',
     stack:["Wordpress","Web Design"],
-    link:""
+    link:"https://www.iglesiadecristolinajereal.org/"
 },
 
 {
     title: "Mi portafolio",
     imgUrl:'https://github.com/DevAndyMacnab/mi-portafolio/blob/main/public/assets/portafolio%20proyecto.png?raw=true',
-    stack:["React.Js","Tailwind","Vite.Js"],
+    stack:["React.Js","Tailwind","Vite"],
     link:"https://github.com/DevAndyMacnab/mi-portafolio"
 },
-{
-    title: "Diseño de Fachada",
-    imgUrl:'https://github.com/DevAndyMacnab/mi-portafolio/blob/main/public/assets/ejemplo.png?raw=true',
-    stack:["Modelado 3D","Blender"],
-    link:""
-},
+
 
 
 ]
